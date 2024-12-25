d4a87cb4efb84f3355cf603f09b3be5e006e5a5d
ImalshaD <plid475@gmail.com>
2024-12-25 17:00:10 +0530

    first commit

---
+++ gktest/__init__.py
@@ -0,0 +1,5 @@
+from .main import Hello
+
+__all__ = [
+    "Hello"
+    ]
---
+++ gktest/main.py
@@ -0,0 +1,3 @@
+class Hello:
+    def __init__(self, name="User"):
+        print(f"Hello, {name} I am Imalsha Puranegedara thanks for using my Repo!")
